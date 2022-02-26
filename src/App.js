f64cb0bca2863258bd473419211475a4208fc641
IMRANDIL <aliimranadil2@gmail.com>
2022-02-26 12:35:01 +0530

    yh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
-function App() {
+import tt from '@tomtom-international/web-sdk-maps';
+
+
+
+
+
+const App = () => {
+  const mapElement = useRef()
+  const [map, setMap] = useState({});
+  const [longitude, setLongitude] = useState(87.186058);
+  const [latitude, setLatitude] = useState(26.053230)
+
+
+
+
+  useEffect(() => {
+    let map = tt.map({
+      key: process.env.REACT_APP_TOM_TOM_KEY,
+      container: mapElement.current,
+      center: [longitude, latitude],
+      zoom: 14
+    });
+
+
+    setMap(map)
+  }, [latitude, longitude])
+
+
+
+
   return (
     <div className="App">
-
+      <div ref={mapElement} className='map'></div>
     </div>
   );
 }
